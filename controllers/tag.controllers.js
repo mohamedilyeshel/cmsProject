fe23366c883c4af4b10d21f3db7b1bc4af650f1e
unknown <mohamedhelalilyes@gmail.com>
2022-09-19 00:25:01 +0100

    used aggregation when getting a tag to check num of followers and stories of that tag

--- controllers/tag.controllers.js
+++ controllers/tag.controllers.js
@@ -1,75 +1,94 @@
+const { default: mongoose } = require("mongoose");
 const tagModel = require("../models/tag.models");
 
-const createTag = async (req, res) => 
-{
-	try 
-    {
-        const newtag = new tagModel({
-            name : req.body.name
-        });
+const createTag = async (req, res) => {
+  try {
+    const newtag = new tagModel({
+      name: req.body.name,
+    });
 
-        const saveTag = await newtag.save();
-        return res.status(200).json(saveTag);
-	} 
-    catch (err)
-    {
-		return res.status(500).json(err);
-	}
+    const saveTag = await newtag.save();
+    return res.status(200).json(saveTag);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 };
 
-const getTags = async (req, res) => 
-{
-	try
-    {
-		const tags = await tagModel.find();
-		return res.status(200).json(tags);
-	} 
-    catch (err) 
-    {
-		return res.status(500).json(err);
-	}
+const getTags = async (req, res) => {
+  try {
+    const tags = await tagModel.find();
+    return res.status(200).json(tags);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 };
 
-const getTag = async (req, res) =>
-{
-	try 
-    {
-		return res.status(200).json(req.tag);
-	} 
-    catch (err) 
-    {
-		return res.status(500).json(err);
-	}
+const getTag = async (req, res) => {
+  try {
+    const tag = await tagModel.aggregate([
+      {
+        $match: {
+          _id: mongoose.Types.ObjectId(req.tag._id),
+        },
+      },
+      {
+        $lookup: {
+          from: "follows",
+          localField: "_id",
+          foreignField: "following.entity",
+          as: "followers",
+        },
+      },
+      {
+        $addFields: {
+          followers: {
+            $size: "$followers",
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: "stories",
+          localField: "_id",
+          foreignField: "tags",
+          as: "stories",
+        },
+      },
+      {
+        $addFields: {
+          stories: {
+            $size: "$stories",
+          },
+        },
+      },
+    ]);
+
+    return res.status(200).json(tag);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 };
 
-const deleteTag = async (req, res) => 
-{
-	const id = req.tag._id;
-	try 
-    {
-		const tag = await tagModel.findByIdAndDelete(id);
-		return res.status(200).json(tag);
-	} 
-    catch (err) 
-    {
-		return res.status(500).json(err);
-	}
+const deleteTag = async (req, res) => {
+  const id = req.tag._id;
+  try {
+    const tag = await tagModel.findByIdAndDelete(id);
+    return res.status(200).json(tag);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 };
 
-const updateTag = async (req, res) => 
-{
-	const id = req.tag._id;
-	try 
-    {
-		const tag = await tagModel.findByIdAndUpdate(id, req.body, {
-			new: true,
-		});
-		return res.status(200).json(tag);
-	} 
-    catch (err) 
-    {
-		return res.status(500).json(err);
-	}
+const updateTag = async (req, res) => {
+  const id = req.tag._id;
+  try {
+    const tag = await tagModel.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    return res.status(200).json(tag);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 };
 
 module.exports.createTag = createTag;
